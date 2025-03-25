8e4e40b20ec56e0703b11f3b6f54d5885bdc479b
ryunitou <ryunitou@gmail.com>
2025-03-25 18:44:44 +0700

    add gh page

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), tailwindcss(), vueDevTools()],
+  base: '/katekima/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
